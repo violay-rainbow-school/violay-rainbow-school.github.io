472b4824ca2d81f385020c4e0c05a53a2d498077
Nicolas Renvoisé <43814948+nicordev@users.noreply.github.com>
2021-02-26 21:29:15 +0100

    use live server url

--- assets/js/reservation/school-api/school-api-config.js
+++ assets/js/reservation/school-api/school-api-config.js
@@ -1,3 +1,3 @@
 // TODO: use liquid variables to set constants
-export const SCHOOL_API_URL = 'http://127.0.0.1:8002/api';
+export const SCHOOL_API_URL = 'https://school-paperwork.nicordev.com/api';
 export const SCHOOL_API_LOGIN_URL = `${SCHOOL_API_URL}/please_login`;
